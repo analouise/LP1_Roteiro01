7d9487b1267066fc504912b0a5853f7c5f223513
Ana Louise <54039039+analouise@users.noreply.github.com>
2019-11-06 21:21:38 -0300

    Questão 03 Roteiro01 - Update

--- exercicio3.cpp
+++ exercicio3.cpp
@@ -7,22 +7,27 @@ using namespace std;
 
 int main(){
     int valor, vsorteado;
-    int total = 0;
-    float arr[6] = {};
+    int total;
     srand(time(NULL)); //Gera valores diferentes
 
     do{
         printf("Digite o valor de jogadas: \n");
         cin >> valor;
-
+        total = 0;
+        float arr[6] = {};
         for(int i = 1; i<=valor; i++){
+
             vsorteado = (rand()%6)+1; //Retorna valor aleatorio de 1 a 6
             arr[vsorteado] = arr[vsorteado]+1;
             total += 1;
+            cout <<"sorteado "<< vsorteado << endl;
+            cout <<arr[vsorteado]<<endl;
+
+        }
+        for(int j=0; j<6; j++){
+            cout << "Face " << j+1 << " tem o percentual de: "<< (arr[j]*100)/total << "%" << endl;
         }
     }while(valor != 0);
 
-    for(int j=1; j<=6; j++){
-        cout << "Face " << j << " tem o percentual de: "<< (arr[j]*100)/total << "%" << endl;
-    }
+
 }
